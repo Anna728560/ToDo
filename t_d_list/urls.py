e315b2b038b51a64e69b8b8448d8a3b60d833705
Polonnikova Anna <xxxuko@gmail.com>
2024-03-11 12:49:51 +0200

    create base structure of pages(hame page with sll tasks/page with all tags)

--- t_d_list/urls.py
+++ t_d_list/urls.py
@@ -4,7 +4,7 @@ from .views import TagsListView, TaskListView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task_list"),
-    path("tags/<int:pk>", TagsListView.as_view(), name="tags_list")
+    path("tags/", TagsListView.as_view(), name="tags_list")
 ]
 
 app_name = "t_d_list"
--- t_d_list/views.py
+++ t_d_list/views.py
@@ -1,16 +1,14 @@
 from django.views import generic
-from django.shortcuts import render
-
 from t_d_list.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
-    template_name = "task_list.html"
+    template_name = "t_d_list/task_list.html"
     context_object_name = "tasks"
 
 
 class TagsListView(generic.ListView):
     model = Tag
-    template_name = 'tags_list.html'
+    template_name = 't_d_list/tags_list.html'
     context_object_name = "tags"
