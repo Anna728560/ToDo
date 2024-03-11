dceb552ae0a50163eea84769b6c7db661dd7bd47
Polonnikova Anna <xxxuko@gmail.com>
2024-03-11 11:04:39 +0200

    create madels and migration

--- t_d_list/models.py
+++ t_d_list/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Todo(models.Model):
+    content = models.TextField()
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    deadline_datetime = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.content
