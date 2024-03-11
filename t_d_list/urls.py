ef27619a6dc6775a5e92b868bb5a3acd37433fd9
Polonnikova Anna <xxxuko@gmail.com>
2024-03-11 13:16:49 +0200

    add update/delete buttons to tags

--- t_d_list/urls.py
+++ t_d_list/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import TagsListView, TaskListView
+from .views import (
+    TagsListView,
+    TaskListView,
+    TagsCreateView,
+    TagsUpdateView,
+    TagsDeleteView,
+)
 
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task_list"),
-    path("tags/", TagsListView.as_view(), name="tags_list")
+    path("tags/", TagsListView.as_view(), name="tags_list"),
+    path("tags/create/", TagsCreateView.as_view(), name="tags_create"),
+    path("tags/update/<int:pk>/", TagsUpdateView.as_view(), name="tags_update"),
+    path("tags/delete/<int:pk>/", TagsDeleteView.as_view(), name="tags_delete"),
 ]
 
 app_name = "t_d_list"
--- t_d_list/views.py
+++ t_d_list/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from t_d_list.models import Task, Tag
+from django.urls import reverse_lazy
 
 
 class TaskListView(generic.ListView):
@@ -12,3 +13,21 @@ class TagsListView(generic.ListView):
     model = Tag
     template_name = 't_d_list/tags_list.html'
     context_object_name = "tags"
+
+
+class TagsCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("t_d_list:tags")
+
+
+class TagsUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("t_d_list:tags")
+
+
+class TagsDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("t_d_list:tags")
+
